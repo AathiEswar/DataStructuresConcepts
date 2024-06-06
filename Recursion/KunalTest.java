91f0ac3a8209e12f4244e9e484c2a55ecda57b68
AathiEswar <aathi28082003@gmail.com>
2024-06-06 21:23:12 +0530

    6-6-2024

--- Recursion/KunalTest.java
+++ Recursion/KunalTest.java
@@ -7,9 +7,107 @@ public class KunalTest {
         //System.out.println(fibonacci(2));
 
         //System.out.println(gcd(180 , 17));
-        int arr[] = {1,2,5,87,99,101};
 
-        System.out.println(binarySearchRecursion(arr , 0 , arr.length-1,101));
+        //int arr[] = {1,2,5,87,99,101};
+        //System.out.println(binarySearchRecursion(arr , 0 , arr.length-1,101));
+
+        //printTillOne(5);
+
+//        System.out.println(factorial(5));
+
+        //System.out.println(sumTillN(100));
+
+        //System.out.println(sumOfDigits(3525));
+
+        //System.out.println(productOfDigits(101));
+
+        //reverseNumber(102023);
+        //System.out.println(sumForReverse);
+
+        //System.out.println(reverseNumberWithHelper(123456));
+
+        System.out.println(numberOfZeros(120001, 0));
+
+    }
+    public int numberOfSteps(int num) {
+        return numberOfStepsHelper(num , 0);
+    }
+
+    public int numberOfStepsHelper(int num , int count){
+        if(num == 0) return count;
+
+        if(num %2 == 0) return numberOfStepsHelper( num / 2 , ++count);
+
+        return numberOfStepsHelper(num-1 , ++count);
+    }
+
+    public static int numberOfZeros(int n , int count){
+        if(n==0) return count;
+
+        int rem = n%10;
+
+        if(rem == 0) return numberOfZeros(n/10 , count+1);
+
+        return numberOfZeros(n/10 , count);
+  }
+
+    // reverse using helper function
+
+    public  static int reverseNumberWithHelper(int n){
+        int digits = (int) Math.log10(n) + 1;
+
+        return reverseNumberHelper(n , digits);
+    }
+
+    private static int reverseNumberHelper(int n , int digits) {
+        if(n%10 == n) return n;
+
+        int unitDigit = n%10;
+
+        return unitDigit * (int)Math.pow(10 , digits-1) + reverseNumberHelper(n/10 , digits-1);
+    }
+
+    // reverse using static variable
+    static  int sumForReverse = 0;
+    public  static void reverseNumber(int n){
+       if(n == 0) return ;
+
+       int digit = n%10;
+       sumForReverse = sumForReverse*10 + digit;
+
+       reverseNumber(n/10);
+    }
+
+    public static int productOfDigits(int n){
+        if(n%10 == n) return n;
+
+        return (n%10) * productOfDigits(n/10);
+    }
+
+    public static int sumOfDigits(int n){
+        if(n == 0)return 0;
+
+        int digit = n%10;
+
+        return digit + sumOfDigits(n/10);
+    }
+
+    public static int sumTillN(int n){
+        if(n<2) return n;
+
+        return n + sumTillN(n-1);
+    }
+    public  static int factorial(int n){
+        if(n==0 )return 1;
+
+        return n * factorial(n-1);
+    }
+    public static void printTillOne(int n){
+        if(n<1){
+            return ;
+        }
+        printTillOne(n-1);
+        System.out.println(n);
     }
 
     public static int binarySearchRecursion(int[] arr , int start , int end , int val){
