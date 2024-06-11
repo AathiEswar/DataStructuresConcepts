39eedd7f3adbc57a232dfbba7ac5e3a1ab34e80d
AathiEswar <aathi28082003@gmail.com>
2024-06-11 21:29:43 +0530

    11-6-2024

--- Recursion/Permutation.java
+++ Recursion/Permutation.java
@@ -8,7 +8,38 @@ public class Permutation {
 
        // System.out.println(permutationStringArrayExtra("abc" , "" , new ArrayList<>()));
 
-        System.out.println(permutationStringArrayOn("abc" ,""));
+       // System.out.println(permutationStringArrayOn("abc" ,""));
+        System.out.println(permutationCountNoExtra("" , "abc"));
+    }
+    public static int permutationCountNoExtra(String p , String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+        char ch = up.charAt(0);
+        int count=0;
+        for (int i = 0; i <= p.length(); i++) {
+            String first = p.substring(0 , i);
+            String last = p.substring(i , p.length());
+
+            count += permutationCount(first + ch + last , up.substring(1) , count);
+        }
+
+        return count;
+    }
+    public static int permutationCount(String p , String up , int count){
+        if(up.isEmpty()){
+            return ++count;
+        }
+        char ch = up.charAt(0);
+
+        for (int i = 0; i <= p.length(); i++) {
+            String first = p.substring(0 , i);
+            String last = p.substring(i , p.length());
+
+         count = permutationCount(first + ch + last , up.substring(1) , count);
+        }
+
+        return count;
     }
 
     public static ArrayList permutationStringArrayOn(String up ,String p){
