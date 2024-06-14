1224c0f75124c5ae4adee3f43900489f1f615f69
AathiEswar <aathi28082003@gmail.com>
2024-06-14 21:49:11 +0530

    14-6-2024

--- LinkedList/Problems.java
+++ LinkedList/Problems.java
@@ -6,6 +6,17 @@ public class Problems {
     public static void main(String[] args) {
 
     }
+    public static ListNode middleOfLinkedList(ListNode head){
+        ListNode fast = head;
+        ListNode slow= head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
 
     public static boolean isHappyNumber(int n){
         int fast = n;
