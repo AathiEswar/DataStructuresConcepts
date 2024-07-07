c6fc316c26e32f5707550d5bdb9d5caa650a8b2d
AathiEswar <aathi28082003@gmail.com>
2024-07-07 10:17:43 +0530

    7-7-24

---
+++ Stacks/InfixToPostFix.java
@@ -0,0 +1,65 @@
+package Concepts.Stacks;
+import java.util.*;
+public class InfixToPostFix {
+    public static void main(String[] args) {
+        System.out.println(infixToPostfix("4 * 5 + 6 / 7 / 8 / 9"));
+    }
+
+    // infix to postfix conversion
+
+    // functions - InToPost , precedenceChecker , precedenceValue , OperatorChecker
+
+    public static String infixToPostfix(String expression){
+        // create necessary ds
+        ArrayList<String> postfixResult = new ArrayList<>();
+
+        // stack to do the operations
+        Stack<String> stack = new Stack<>();
+
+        // split and iterate through the text
+        for(String chr : expression.split(" ")){
+            // if operator need to perform some operations
+            if(isOperator(chr)){
+                // is the stack is not empty and the peek of stack has higher precedence then pop the peek
+                while(!stack.isEmpty() && hasLowerPrecedence(chr , stack.peek())){
+                    postfixResult.add(stack.pop());
+                }
+                // add the cur op to stack for further processing
+                stack.push(chr);
+            }
+            // if number then just add to the result list
+            else{
+                postfixResult.add(chr);
+            }
+        }
+        // the stack might not be empty yet so add everything to the result list
+        while(!stack.isEmpty()){
+            postfixResult.add(stack.pop());
+        }
+
+        // return the resultant list as a string
+        return String.join(" " , postfixResult);
+    }
+
+    public static boolean isOperator(String character){
+        // create a list of op and return if they contain the operator
+        return List.of("+" , "-" , "/" ,"*" , "%").contains(character);
+    }
+
+    public static boolean hasLowerPrecedence(String op1 , String op2){
+        // check if the precedence in the top is greater than the cur operator
+        return precedenceValue(op1) < precedenceValue(op2);
+    }
+
+    public static int precedenceValue(String op){
+        // switch can return the value
+
+        // return the value of precedence for the precedence checker
+        return switch (op){
+            case "+" , "-" -> 1;
+            case "*" , "/" -> 2;
+            case "%" -> 3;
+            default -> 4;
+        };
+    }
+}
