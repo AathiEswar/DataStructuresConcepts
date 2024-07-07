449c573301749f9380a29fe7dbc56651c2d52f46
AathiEswar <aathi28082003@gmail.com>
2024-07-07 14:49:09 +0530

    7-7-2024

--- Stacks/InfixToPrefix.java
+++ Stacks/InfixToPrefix.java
@@ -1,16 +1,20 @@
 package Concepts.Stacks;
-import java.util.*;
-public class InfixToPostFix {
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class InfixToPrefix {
     public static void main(String[] args) {
-        String result = infixToPostfix("4 * ( 5 + 10 ) + 2");
+        String result = infixToPrefix("4 * 5 + 10 / 2");
         System.out.println(result);
-        System.out.println(evaluatePostfixExpression(result));
+        System.out.println(evaluatePrefixExpression(result));
     }
 
-    // infix to postfix conversion
+    // infix to prefix conversion
 
     // functions - InToPost , precedenceChecker , precedenceValue , OperatorChecker
-    public static int evaluatePostfixExpression(String expression){
+    public static int evaluatePrefixExpression(String expression){
         // create necessary ds
         Stack<Integer> stack = new Stack<>();
 
@@ -38,48 +42,36 @@ public class InfixToPostFix {
 
         return stack.pop();
     }
-    public static String infixToPostfix(String expression){
+    public static String infixToPrefix(String expression){
         // create necessary ds
-        ArrayList<String> postfixResult = new ArrayList<>();
+        ArrayList<String> prefixResult = new ArrayList<>();
 
         // stack to do the operations
         Stack<String> stack = new Stack<>();
 
         // split and iterate through the text
         for(String chr : expression.split(" ")){
-            if(chr.equals("(")){
-                stack.push("(");
-            }
-            else if(chr.equals(")")){
-               while(!stack.isEmpty() && stack.peek()!= "("){
-                   postfixResult.add(stack.pop());
-
-               }
-               if(!stack.isEmpty()){
-                   stack.pop();
-               }
-            }
             // if operator need to perform some operations
-            else if(isOperator(chr)){
+            if(isOperator(chr)){
                 // is the stack is not empty and the peek of stack has higher precedence then pop the peek
                 while(!stack.isEmpty() && hasLowerPrecedence(chr , stack.peek())){
-                    postfixResult.add(stack.pop());
+                    prefixResult.add(stack.pop());
                 }
                 // add the cur op to stack for further processing
                 stack.push(chr);
             }
             // if number then just add to the result list
             else{
-                postfixResult.add(chr);
+                prefixResult.add(chr);
             }
         }
         // the stack might not be empty yet so add everything to the result list
         while(!stack.isEmpty()){
-            postfixResult.add(stack.pop());
+            prefixResult.add(stack.pop());
         }
 
         // return the resultant list as a string
-        return String.join(" " , postfixResult);
+        return String.join(" " , prefixResult);
     }
 
     public static boolean isOperator(String character){
@@ -100,7 +92,7 @@ public class InfixToPostFix {
             case "+" , "-" -> 1;
             case "*" , "/" -> 2;
             case "%" -> 3;
-            default -> -1 ;
+            default -> 4;
         };
     }
 }
