8691cfbe73c7f6b0614859fb1ae5772fb3b8423e
AathiEswar <aathi28082003@gmail.com>
2024-07-25 14:02:33 +0530

    25--7-2024

--- SortingAndSearch/CountingSort.java
+++ SortingAndSearch/CountingSort.java
@@ -11,6 +11,40 @@ public class CountingSort {
         System.out.println(Arrays.toString(arr));
     }
 
+    public static void countingSortNegative(int[] nums){
+        int max = nums[0];
+        int min = nums[0];
+
+        for(int num : nums){
+            if(num > max){
+                max = num;
+            }
+            if(num < min){
+                min = num;
+            }
+        }
+
+        int len = max - min ;
+        int[] freq = new int[len+1];
+
+        for(int num : nums){
+            freq[num - min]++;
+        }
+
+        int index = 0 ;
+        for(int i = 0 ; i <= len  ; i++){
+            while(freq[i] > 0 ){
+                nums[index] = min;
+                index++;
+                freq[i]--;
+            }
+            min++;
+        }
+
+        return nums;
+    }
+
+    // FOR small positive number
     private static void countingSort(int[] arr) {
         // find max
         // create an array with max+1 array
@@ -32,7 +66,7 @@ public class CountingSort {
         }
 
         int index = 0 ;
-        for(int i = 0 ; i<max;i++){
+        for(int i = 0 ; i<=max;i++){
             while(freq[i] >0){
                 arr[index] = i;
                 index++;
